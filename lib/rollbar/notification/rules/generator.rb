bc64cca539c2d75d8ad666e5bd2aa26b22b3fcb5
abicky <takeshi.arabiki@gmail.com>
2022-12-11 10:50:09 +0900

    Initialize with "bundle gem rollbar-notification-rules-generator --exe --no-coc --mit"

---
+++ lib/rollbar/notification/rules/generator.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "generator/version"
+
+module Rollbar
+  module Notification
+    module Rules
+      module Generator
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/rollbar/notification/rules/generator/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Rollbar
+  module Notification
+    module Rules
+      module Generator
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
